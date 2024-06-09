016e3427361e62921c7b860cb7fe9d94679fc853
AhmedTarekEl-Mahdy <ahmedtarek15197@gmail.com>
2024-06-09 00:26:05 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point.
+ *
+ * Description: A C program that exactly "Programming is
+ * like building a multilingual puzzle",
+ * Followed by a new line,
+ * Using function puts().
+ *
+ * Return: Always 0 (Success).
+ */
+
+int main(void)
+{
+	printf("Programming is like building a multilingual puzzle\n");
+
+	return (0);
+}
