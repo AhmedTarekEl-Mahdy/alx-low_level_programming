cc79c198ff9f595a19c0f0b7022e7d61776603b0
AhmedTarekEl-Mahdy <ahmedtarek15197@gmail.com>
2024-06-09 00:39:39 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point.
+ *
+ * Description: A C program that prints exactly "with proper gramar,
+ * but the outcome is a piece of art," followed
+ * by a new line,
+ * Using the function printf().
+ *
+ * Return: Always 0 (Success).
+ */
+
+int main(void)
+{
+	printf("with proper gramar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
