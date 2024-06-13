4200ed10cad2d093bf0fa77e2c9b50179d398b69
AhmedTarekEl-Mahdy <ahmedtarek15197@gmail.com>
2024-06-13 05:35:07 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point.
+ *
+ * Description: A C program that prints exactly with proper gramer\,
+ * but the outcome is a piece of art, .
+ * Followed by new line,
+ * Using the function printf() only,
+ * You are not allowed to use the function puts,
+ * Your program should return 0,
+ * Your program should compile without warning when using the
+ * -Wall gcc option.
+ *
+ *  Return: Always 0 (Success).
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
