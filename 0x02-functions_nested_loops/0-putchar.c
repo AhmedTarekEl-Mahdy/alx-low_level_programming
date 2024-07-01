400574df06d037d22644cf4761c856a8c42160e9
AhmedTarekEl-Mahdy <ahmedtarek15197@gmail.com>
2024-06-30 18:54:34 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point.
+ *
+ * Description: A program that prints _putchar,
+ * followed by a new line.
+ *
+ * Return: Always 0 (Success).
+ */
+
+int main(void)
+{
+	puts("_putchar");
+
+	return (0);
+}
